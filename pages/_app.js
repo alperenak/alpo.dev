82a71e2dcb2d1860f4aa6c1516ef564dc319e239
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-06-26 03:44:17 +0300

    fix import

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import './index.css'
 import React from 'react'
-import Logo from './components/page1/logo'
-import LogoName from './components/page1/logoName'
-import Background from './components/page1/background'
+import Logo from './components/page 1/logo'
+import LogoName from './components/page 1/logoName'
+import Background from './components/page 1/background'
 export default function App() {
     return (
         <div>
