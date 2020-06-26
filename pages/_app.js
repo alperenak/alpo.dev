354df9cd538bc9a133250a3c025dea5c0442e9b5
alperenkaraguzel <alperenkaraguzel@gmail.com>
2020-06-26 03:57:50 +0300

    navbar added

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Logo from './components/page 1/logo'
 import LogoName from './components/page 1/logoName'
 import Background from './components/page 1/background'
+import Nav from './components/page 1/nav'
 export default function App() {
     return (
-        <div>
+        <div className="container">
             <Background />
             <LogoName />
             <Logo />
+            <Nav/>
         </div>
     )
 }
--- pages/components/page 1/nav.js
+++ pages/components/page 1/nav.js
@@ -4,7 +4,10 @@ export default function Nav() {
 
     return (
         <div className='navbar'>
-
+            <p>BLOG</p>
+            <p>RESUME</p>
+            <p>ABOUT</p>
+            <div className="nav-contact">CONTACT</div>
         </div>
     )
 
